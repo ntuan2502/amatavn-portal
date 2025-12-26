32f1c18fe2043c9cb1927e4a3f829b94ce453f73
Tún <ntuan.2502@gmail.com>
2025-12-26 16:16:48 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,77 @@
-import Image from "next/image";
+import React from "react";
+import {
+  DraggableCardBody,
+  DraggableCardContainer,
+} from "@/components/ui/draggable-card";
+import Link from "next/link";
 
 export default function Home() {
+  const items = [
+    {
+      title: "EZ Order",
+      image: "https://portal.amata.com/vn/web/image/ezorder.jpg",
+      link: "https://apps.powerapps.com/play/e/default-06d2bc4d-7325-430e-808f-ecfe9126a217/a/58d921fe-51e6-444f-bd3d-883346ead46a?tenantId=06d2bc4d-7325-430e-808f-ecfe9126a217&hint=8513fb94-d586-4c87-83df-bd4d5c551e0b&sourcetime=1749457509877&source=portal&hidenavbar=true",
+    },
+    {
+      title: "IT Ticket",
+      image: "https://portal.amata.com/vn/web/image/ticket.jpg",
+      className: "absolute top-40 left-[25%] rotate-[-7deg]",
+      link: "https://mis.amata.com/ticket/login.aspx",
+    },
+    {
+      title: "HRIS",
+      image: "https://portal.amata.com/vn/web/image/hris_staff.jpg",
+      className: "absolute top-5 left-[40%] rotate-[8deg]",
+      link: "http://portalvn.amata.com:9669/New_Login/New_Index",
+    },
+    {
+      title: "VN Intranet",
+      image:
+        "https://vnintranet.amata.com/intranet/wp-content/uploads/2024/12/bg2.png",
+      className: "absolute top-32 left-[55%] rotate-[10deg]",
+      link: "https://vnintranet.amata.com/intranet/",
+
+    },
+    {
+      title: "Online Approval",
+      image:
+        "https://portal.amata.com/vn/web/image/onlineapprove.jpg",
+      className: "absolute top-20 right-[35%] rotate-[2deg]",
+      link: "https://mis.amata.com/online_approval/",
+    },
+    {
+      title: "Car Booking",
+      image:
+        "https://portal.amata.com/vn/web/image/car_booking.jpg",
+      className: "absolute top-24 left-[45%] rotate-[-7deg]",
+      link: "https://mis.amata.com/car_booking/login.aspx",
+    },
+    // {
+    //   title: "Canada",
+    //   image:
+    //     "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    //   className: "absolute top-8 left-[30%] rotate-[4deg]",
+    // },
+  ];
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <DraggableCardContainer className="relative flex min-h-screen w-full items-center justify-center overflow-clip">
+      <p className="absolute top-1/2 mx-auto max-w-sm -translate-y-3/4 text-center text-2xl font-black text-neutral-400 md:text-4xl dark:text-neutral-800">
+        AMATA VN PORTAL
+      </p>
+      {items.map((item) => (
+        <DraggableCardBody className={item.className} key={item.title}>
+          <img
+            src={item.image}
+            alt={item.title}
+            className="pointer-events-none relative z-10 h-80 w-80 object-cover"
+          />
+          <Link href={item.link}>
+            <h3 className="mt-4 text-center text-2xl font-bold text-neutral-700 dark:text-neutral-300">
+              {item.title}
+            </h3>
+          </Link>
+        </DraggableCardBody>
+      ))}
+    </DraggableCardContainer>
   );
 }
